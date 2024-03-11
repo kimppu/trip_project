6a24351d8e7d1b92ceb16f3be64ba257fb8dc7dd
Playdata <chelirt13@naver.com>
2024-03-11 19:16:14 +0900

    add: login

--- src/main/java/com/example/travelproject/domain/mainpage/controller/MainController.java
+++ src/main/java/com/example/travelproject/domain/mainpage/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.example.travelproject.domain.mainpage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -9,19 +11,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.travelproject.domain.user.model.UserEntity;
 import com.example.travelproject.domain.user.service.UserService;
 
+
 @Controller
 public class MainController {
+    
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
-    public RedirectView redirectToMain() {
-        return new RedirectView("/main");
+    /*
+     * 누구나 접근 가능
+     */
+    @GetMapping("/index")
+    public String index(Authentication authentication, Model model) {
+        if(authentication != null) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            model.addAttribute("menuTitle", "홈");
+        }
+        model.addAttribute("menuTitle", "홈");
+        return "index";
     }
 
     @GetMapping("/loginPage")
@@ -42,25 +53,51 @@ public class MainController {
         userService.joinUserDto(dto);
         return "redirect:/loginPage";
     }
-
-    @GetMapping("/userPage")
+    
+    /*
+     * 로그인한 경우만 
+     */
+    @GetMapping("/user/index")
     public String user(Authentication authentication, Model model) {
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         model.addAttribute("username", userDetails.getUsername());
-        return "login/userPage";
+        model.addAttribute("menuTitle", "홈");
+        return "staff/user";
     }
 
+    @GetMapping("/manager/index")
+    public String manager(Authentication authentication, Model model) {
 
-    @GetMapping("/main")
-    public String mainIndex(Authentication authentication, Model model){
-        if(authentication != null) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            model.addAttribute("username", userDetails.getUsername());
-            
-        }
-        model.addAttribute("menuTitle","홈");
-        return "index";
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("username", userDetails.getUsername());
+        return "staff/manager1";
+    }
+
+    @GetMapping("/admin/index")
+    public String admin(Authentication authentication, Model model) {
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("username", userDetails.getUsername());
+        return "staff/admin1";
+    }
+
+    @Secured("ADMIN")
+    @GetMapping("/secured")
+    public String secured(Authentication authentication, Model model) {
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("username", userDetails.getUsername());
+        return "staff/secured";
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER')")
+    @GetMapping("/secured-roles")
+    public String securedRoles(Authentication authentication, Model model) {
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("username", userDetails.getUsername());
+        return "staff/securedRoles";
     }
 
 }
